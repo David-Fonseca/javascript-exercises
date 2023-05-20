31fe79fd1b87ad60fc53f27acd4191d177ed2976
davidfonseca56 <49604760+davidfonseca56@users.noreply.github.com>
2023-05-20 00:54:48 -0600

    solved 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...args) {
+    for (let arg of args){
+        for (let i = 0; i<list.length; i++){
+            if (list[i] === arg){
+                list.splice(i,1);
+            }
+        }     
+    }
 
+    return list;
 };
+
+
+console.log(removeFromArray([1, 2, 3], "1", 3))
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Math.round(10*((temp - 32)*5/9))/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(10*(temp*(9/5) + 32))/10;
 };
 
+console.log(convertToCelsius(-100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
